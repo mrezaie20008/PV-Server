1dbe24fb2881c9149bee1716c56060c065942fb9
mrezaie20008 <mohammadrezaie20081387@gmail.com>
2025-08-29 15:16:23 +0000

    modified:   flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    __html = open(Path(HOME_DIR).joinpath("templates", "welcome", "index.html"), "rb").read()
+    __html = get_html(Path(HOME_DIR).joinpath("templates", "welcome", "index.html"))
     return __html
 
