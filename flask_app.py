2f984d5f8b01d933175f248c78e71ad915868633
mrezaie20008 <mohammadrezaie20081387@gmail.com>
2025-08-31 10:30:03 +0500

    modified:   .gitignore 	modified:   flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from pathlib import Path
-from configs import HOME_DIR
+from configs import *
 
-
-
+# App Configurations
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    __html = open(Path(HOME_DIR).joinpath("templates", "welcome", "index.html"), "rb").read()
+    __html = get_html(Path(HOME_DIR).joinpath("templates", "welcome", "index.html"))
 
     return __html
+
+@app.route("vids/<vid>")
+def get_vid(vid):
+    return f"<h1> Are you looking for {vid} video?</h1>"
 
