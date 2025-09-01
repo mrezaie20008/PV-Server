e8c4a77f35a9956261d34f0f961b0c675b6285c9
mrezaie20008 <mohammadrezaie20081387@gmail.com>
2025-09-01 12:39:49 +0500

    modified:   flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -11,15 +11,27 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
+# Error Pages
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+# Routes / Pages
 @app.route('/')
 def hello_world():
     return render_template("welcome/index.html", data="Mohammad")
 
+
 @app.route("/vids")
 def show_vids():
-    return ", ".join(list_vids(VID_DIR))
+    return ", ".join(list_vids())
 
-@app.route("/vids/<vid>")
+
+@app.get("/vids/<vid>")
 def get_vid(vid):
+    if not vid in list_vids():
+        return f"<h1>Video Not Found</h1>"
+
     return f"<h1> Are you looking for {escape(vid)} video?</h1>"
 
